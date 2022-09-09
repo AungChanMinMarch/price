fc089184c3e7392da48bd51502edff5ac25b11e7
AungChanMin293 <aungchanmin293@gmail.com>
2022-09-09 21:57:26 +0630

    intial hosting

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const webpack = require('webpack')
 const path = require('path');
 
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
 
     entry: './src/index.js',
@@ -42,6 +44,13 @@ module.exports = {
     //             use: ['style-loader','css-loader']
     //         }
         ]
-    }
+    },
 
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: `index.html`,
+            template: `./public/index.html`,
+            inject: "body",
+        }),
+    ]
 }
