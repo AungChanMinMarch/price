4bcc908b6bf76b67e2a6fea519fc3f2b57b12484
AungChanMin293 <aungchanmin293@gmail.com>
2022-09-14 00:20:32 +0630

    added and removed .env

---
+++ models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+	email:  {
+        type: String,
+        unique: [true, "email has already exists in database!"],
+        lowercase: true,
+        trim: true,
+        required: [true, "email not provided"],
+        validate: {
+          validator: function (v) {
+            return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(v);
+          },
+          message: '{VALUE} is not a valid email!'
+        }
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+// userSchema.index({ email: 1 }, { unique: true})
+
+module.exports = mongoose.model("User", userSchema)
